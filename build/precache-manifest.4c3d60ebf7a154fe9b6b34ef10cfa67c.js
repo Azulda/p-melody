9e74ad98462a6640c13c4ebff45a48513e38132a
Azulda <azuldacian@gmail.com>
2020-09-20 18:54:00 -0300

    master

---
+++ build/precache-manifest.4c3d60ebf7a154fe9b6b34ef10cfa67c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3495bc3a702606800341cd31c8fe7d61",
+    "url": "/p-melody/index.html"
+  },
+  {
+    "revision": "6028fcdf103554c12de7",
+    "url": "/p-melody/static/css/main.9a2e813c.chunk.css"
+  },
+  {
+    "revision": "a08b5082d5b335da16bd",
+    "url": "/p-melody/static/js/2.effdc806.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/p-melody/static/js/2.effdc806.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6028fcdf103554c12de7",
+    "url": "/p-melody/static/js/main.55b18af4.chunk.js"
+  },
+  {
+    "revision": "0ee3d10965e580360daf",
+    "url": "/p-melody/static/js/runtime-main.8c095bd0.js"
+  }
+]);
